933e8871dec327acf2b68c07015db04f2c2cf136
ladykakki <greta_calamari@hotmail.it>
2022-08-29 18:13:47 +0200

    prova

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacto
+          Learn Reactooo
         </a>
       </header>
     </div>
