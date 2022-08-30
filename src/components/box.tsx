89578e486befde862f433eb418a145327b8259b0
ladykakki <greta_calamari@hotmail.it>
2022-08-30 14:44:14 +0200

    typescript e aggiunta cambio inserisci voce

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -2,9 +2,18 @@
 import '../css/style.css';
 import 'animate.css';
 import {useState} from 'react';
+   
+    // @ts-ignore
+
 import { ReactComponent as YourSvg } from '../img/Bin.svg';
+    // @ts-ignore
+
 import { ReactComponent as Add } from '../img/Adds.svg';
+    // @ts-ignore
+
 import { ReactComponent as Off } from '../img/Checkbox_Off.svg';
+    // @ts-ignore
+
 import { ReactComponent as On } from '../img/Checkbox_On.svg';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
@@ -16,8 +25,6 @@ import 'reactjs-popup/dist/index.css';
 
 function Box(){
   
-  // const [showForm, setFormStatus] = useState(false);
-  // const viewData = () => setFormStatus(true);
 
 
   
@@ -26,24 +33,33 @@ function Box(){
 
   function handleClick() {
     setButtonText('Seconda Voce');
+    
   }
 
 
   function handleAdd(){
-    document.getElementById('n').style.display = "block";
+    // @ts-ignore
+   document.getElementById('n').style.display = "block";
+   setButtonText('Inserisci voce')
   }
   
   function handleDelete(){
+    // @ts-ignore
     document.getElementById('ab').style.display = "none";
+    // @ts-ignore
     document.getElementById('s').style.display = "block";
 
   }
   function handleBin(){
+    // @ts-ignore
     document.getElementById('n').style.display = "none";
     const oggetto1 = document.getElementById('s');
+    // @ts-ignore
     oggetto1.classList.remove('animate__animated', 'animate__fadeInDown');
     const oggetto2 = document.getElementById('c');
+    // @ts-ignore
     oggetto1.classList.add('animate__animated', 'animate__fadeInDown');
+    // @ts-ignore
     oggetto2.classList.add('animate__animated', 'animate__fadeInDown');
 
 
@@ -64,32 +80,51 @@ function Box(){
 
   
     return(
+      //@ts-ignore
       <div className="container my-container">
+     {/* @ts-ignore */}
+      
         <div className="box">
+          {/* @ts-ignore */}
           <div className='l-box'>
+            {/* @ts-ignore */}
               <h1 className="title">Titolo</h1>
-
+              {/* @ts-ignore */}
               <div id="ab">
+                {/* @ts-ignore */}
                 <YourSvg  style={{width:'30px'}}></YourSvg>
+                {/* @ts-ignore */}
                 <Off onClick={handleDelete} style={{width:'30px'}} className="mx-4 my-1" > </Off>
+                {/* @ts-ignore */}
                 <span  style={{color:'white'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim non risus quis mauris aliquam quis</span>
               </div>
 
-
+              {/* @ts-ignore */}
               <div className='none animate__animated animate__fadeInUp' id="n"> 
+              {/* @ts-ignore */}
                <YourSvg onClick={handleBin} style={{width:'30px'}}></YourSvg>
+               {/* @ts-ignore */}
                <Off style={{width:'30px'}} className="mx-4 mb-1"></Off>
+               {/* @ts-ignore */}
                <span style={{color:'white'}}>Seconda voce</span>
               </div>
 
+              {/* @ts-ignore */}
               <div className='none animate__animated animate__fadeInDown' id="s">
+                {/* @ts-ignore */}
                 <YourSvg  style={{width:'30px'}}></YourSvg>
+                {/* @ts-ignore */}
                 <On style={{width:'30px'}} className="mx-4 my-2"></On>
+                {/* @ts-ignore */}
                 <span style={{color:'white'}} className='barred'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim non risus quis mauris aliquam quis</span>
               </div>
+              {/* @ts-ignore */}
               <div id="c">
+                {/* @ts-ignore */}
                 <YourSvg style={{width:'30px'}}></YourSvg>
+                {/* @ts-ignore */}
                 <On style={{width:'30px'}} className="mx-4 "></On>
+                {/* @ts-ignore */}
                 <span style={{color:'white'}} className='barred'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim non risus quis mauris aliquam quis</span>
               </div>
 
@@ -97,21 +132,27 @@ function Box(){
 
           </div>
         </div>
+        {/* @ts-ignore */}
         <div className="b-space">
           
-         
+         {/* @ts-ignore */}
           <Popup trigger={<button className='btn my-btn'>
+            {/* @ts-ignore */}
             <Add className='plus ms-3' style={{width:'23px'}}></Add>
+            {/* @ts-ignore */}
             <span style={{fontSize:'15px',fontWeight:'600'}} className="mt-2 mx-3 sub">Nuova Voce</span>
           </button>} 
           modal
           >
+            {/* @ts-ignore */}
             {close => (
-            
+            //@ts-ignore
               <div className='animate__animated animate__fadeInUp form-square'>
-                
+                {/* @ts-ignore */}
                 <p onClick={handleClick}  className='distance-list' style={{color:'white'}}>{buttonText}</p> 
+                {/* @ts-ignore */}
                 <div className='distance'>
+                  {/* @ts-ignore */}
                   <button  
                   className='btn my-btn2' 
                   type="submit" 
@@ -127,31 +168,20 @@ function Box(){
            
 
             </div>
+            
           
           )}
+          
           </Popup>
         </div>
-        
-
-        {/* {showForm && (
-        <div className='animate__animated animate__fadeInUp form-square'>
-            
-           <p className='' style={{color:'white'}}>inserisci voce</p> 
-            <div className='distance'>
-            <button className='btn my-btn2' type="submit" style={{fontSize:'15px',fontWeight:'600'}}>Salva</button>
-            </div>
-          
-        </div>
-
-        
-      )} */}
-
-
-
       </div>
     );
 }
 export default Box;
+      
+
+
+
 
 
         
